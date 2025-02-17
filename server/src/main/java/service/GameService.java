28738851e97612504b4f4cf9223b2d74002716ba
Joshua Gneitng - SkyTech <gneitingj9@gmail.com>
2025-02-17 13:56:22 -0700

    Created stub service classes and request/return record classes

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,9 @@
+package service;
+
+import model.*;
+
+public class GameService {
+    public ListGamesResult listGames(ListGamesRequest listRequest) {throw new UnsupportedOperationException();}
+    public NewGameResult newGame(NewGameRequest newGameRequest) {throw new UnsupportedOperationException();}
+    public void joinGame(JoinGameRequest joinRequest) {throw new UnsupportedOperationException();}
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,10 @@
+package service;
+
+import jdk.jshell.spi.ExecutionControl;
+import model.*;
+
+public class UserService {
+    public RegisterResult register(RegisterRequest registerRequest) {throw new UnsupportedOperationException();}
+    public LoginResult login(LoginRequest loginRequest) {throw new UnsupportedOperationException();}
+    public void logout(LogoutRequest logoutRequest) {throw new UnsupportedOperationException();}
+}
