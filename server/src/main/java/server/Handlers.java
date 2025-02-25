af2c812f9542349e900c460b10be38b45fc9bde5
Joshua Gneitng - SkyTech <gneitingj9@gmail.com>
2025-02-25 10:33:44 -0700

    Created endpoints and handler stubs

---
+++ server/src/main/java/server/Handlers.java
@@ -0,0 +1,34 @@
+package server;
+
+import service.ServiceError;
+import spark.*;
+
+public class Handlers {
+    public static String clearApplication(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+
+    public static String register(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+
+    public static String login(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+
+    public static String logout(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+
+    public static String listGames(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+
+    public static String createGame(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+
+    public static String joinGame(Request request, Response response) {
+        throw new ServiceError("Not implemented", 500);
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
+import service.ServiceError;
 import spark.*;
+
+import static server.Handlers.*;
 
 public class Server {
 
@@ -10,9 +13,23 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", Handlers::clearApplication);
+        Spark.post("/user", Handlers::register);
+        Spark.post("/session", Handlers::login);
+        Spark.delete("/session", Handlers::logout);
+        Spark.get("/game", Handlers::listGames);
+        Spark.post("/game", Handlers::createGame);
+        Spark.put("/game", Handlers::joinGame);
+
+        // Handle Service error exception
+        Spark.exception(ServiceError.class, (exception, request, response) -> {
+            response.status(exception.getCode());
+            response.type("application/json");
+            response.body("{\"message\": \"" + exception.getMessage() + "\"}");
+        });
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+//        Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
