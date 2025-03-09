0f8296179f6bf66292666de37df46fe58ebd2afa
Joshua Gneitng - SkyTech <gneitingj9@gmail.com>
2025-03-09 11:40:11 -0600

    Implement SQL user DAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,20 +1,81 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
+import java.sql.SQLException;
+
+import static dataaccess.DatabaseManager.createDatabase;
+import static dataaccess.DatabaseManager.getConnection;
+
 public class SQLUserDAO implements UserDAO{
-    @Override
-    public UserData getUser(String username) {
-        return null;
+
+    private final String[] createStatement =  {
+            """
+            CREATE TABLE IF NOT EXISTS user (
+                `username` varchar(32) NOT NULL,
+                `email` varchar(64) NOT NULL,
+                `password` varchar(256) NOT NULL,
+                PRIMARY KEY (`username`)
+            )
+            """
+    };
+
+    public SQLUserDAO() throws DataAccessException {
+        createDatabase();
+        try (var conn = getConnection()) {
+            for (var statement : createStatement) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Could not create auth table");
+        }
     }
 
     @Override
-    public void createUser(UserData user) {
-
+    public UserData getUser(String username) throws DataAccessException{
+        try (var conn = getConnection()) {
+            var statement = "SELECT username, email, password FROM user where username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1,username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new UserData(username, rs.getString("password"), rs.getString("email"));
+                    }
+                    return null;
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Could not get user");
+        }
     }
 
     @Override
-    public void clear() {
+    public void createUser(UserData user) throws DataAccessException {
+        try (var conn = getConnection()) {
+            var statement = "INSERT INTO auth (username, email, password) VALUES (?, ?, ?)";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, user.username());
+                ps.setString(2, user.email());
+                ps.setString(3, user.password());
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Could not create user");
+        }
+    }
 
+    @Override
+    public void clear() throws DataAccessException{
+        try (var conn = getConnection()) {
+            var statement = "TRUNCATE user";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Could not clear user table");
+        }
     }
 }
