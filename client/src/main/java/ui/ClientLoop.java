72cba16b5f2d0411bfe4086a8bac986ca520221a
Joshua Gneitng - SkyTech <gneitingj9@gmail.com>
2025-03-24 01:03:43 -0600

    Implemented displaying the chessboard from both perspectives

--- client/src/main/java/ui/ClientLoop.java
+++ client/src/main/java/ui/ClientLoop.java
@@ -1,7 +1,14 @@
 package ui;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import model.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 
 import static ui.EscapeSequences.*;
@@ -17,6 +24,7 @@ public class ClientLoop {
     private String authToken;
     ServerFacade facade;
     HashMap<Integer, Integer> gameMap = new HashMap<>();
+    String joinRole;
 
     private final String noauth =
             String.format("""
@@ -42,6 +50,34 @@ public class ClientLoop {
             SET_TEXT_COLOR_GREEN, SET_TEXT_COLOR_BLUE, SET_TEXT_COLOR_GREEN, SET_TEXT_COLOR_BLUE,
             SET_TEXT_COLOR_GREEN, SET_TEXT_COLOR_BLUE, RESET_TEXT_COLOR);
 
+    private final String[] rowLabels = {
+        "A", "B", "C", "D", "E", "F", "G", "H"
+    };
+
+    private final String[] columnLabels = {
+        "8", "7", "6", "5", "4", "3", "2", "1"
+    };
+
+    private final HashMap<ChessPiece.PieceType, String> whitePieceMap = new HashMap<>() {{
+        put(ChessPiece.PieceType.KING, WHITE_KING);
+        put(ChessPiece.PieceType.QUEEN, WHITE_QUEEN);
+        put(ChessPiece.PieceType.BISHOP, WHITE_BISHOP);
+        put(ChessPiece.PieceType.KNIGHT, WHITE_KNIGHT);
+        put(ChessPiece.PieceType.ROOK, WHITE_ROOK);
+        put(ChessPiece.PieceType.PAWN, WHITE_PAWN);
+        put(null, EMPTY);
+    }};
+
+    private final HashMap<ChessPiece.PieceType, String> blackPieceMap = new HashMap<>() {{
+        put(ChessPiece.PieceType.KING, BLACK_KING);
+        put(ChessPiece.PieceType.QUEEN, BLACK_QUEEN);
+        put(ChessPiece.PieceType.BISHOP, BLACK_BISHOP);
+        put(ChessPiece.PieceType.KNIGHT, BLACK_KNIGHT);
+        put(ChessPiece.PieceType.ROOK, BLACK_ROOK);
+        put(ChessPiece.PieceType.PAWN, BLACK_PAWN);
+        put(null, EMPTY);
+    }};
+
     private String[] parseCommand(String in) {
         return in.split(" ");
     }
@@ -55,8 +91,10 @@ public class ClientLoop {
         String input = "";
         var state = UIState.LOG_OUT;
         while (state != UIState.QUIT) {
-            displayStateString(state);
-            input = System.console().readLine();
+            if (state != UIState.GAMEPLAY) {
+                displayStateString(state);
+                input = System.console().readLine();
+            }
             try {
                 state = switch (state) {
                     case LOG_OUT:
@@ -64,7 +102,7 @@ public class ClientLoop {
                     case LOG_IN:
                         yield loginOptions(input);
                     case GAMEPLAY:
-
+                        yield gameplayOptions(input);
                     default:
                         yield state;
                 };
@@ -198,11 +236,23 @@ public class ClientLoop {
                 // Join the game
                 JoinGameRequest joinRequest = new JoinGameRequest(authToken, command[2], gameID);
                 facade.joinGame(joinRequest);
+                joinRole = command[2];
                 yield UIState.GAMEPLAY;
             case "observe":
-                // Not implemented yet
-                System.out.println(SET_TEXT_COLOR_RED + "Not implemented yet." + RESET_TEXT_COLOR);
-                yield UIState.LOG_IN;
+                // Expect exactly 2 arguments
+                if (command.length != 2) {
+                    throw new IllegalArgumentException("Invalid number of arguments. Expected 2.");
+                }
+                // Game ID must be in the gameMap
+                if (!gameMap.containsKey(Integer.parseInt(command[1]))) {
+                    throw new IllegalArgumentException("Invalid game ID.");
+                }
+
+                // Observe not implemented
+                System.out.println(SET_TEXT_COLOR_RED + "Observe not implemented" + RESET_TEXT_COLOR);
+                joinRole = "OBSERVER";
+
+                yield UIState.GAMEPLAY;
             case "logout":
                 // Expect exactly one argument
                 if (command.length != 1) {
@@ -228,4 +278,94 @@ public class ClientLoop {
                 yield UIState.LOG_IN;
         };
     }
+
+    private UIState gameplayOptions(String in) {
+        // For now, draw a new chessGame in proper orientation
+        ChessGame game = new ChessGame();
+        boolean white = !joinRole.equals("BLACK");
+
+        // Convert board to list of strings
+        Collection<String> board = convertBoard(game, joinRole);
+
+        // Generate the view row and column label lists
+        String[] viewRowLabels;
+        String[] viewColumnLabels;
+        if (white) {
+            viewRowLabels = rowLabels;
+            viewColumnLabels = columnLabels;
+        } else {
+            viewRowLabels = new String[8];
+            viewColumnLabels = new String[8];
+            for (int i = 0; i < 8; i++) {
+                viewRowLabels[i] = rowLabels[7 - i];
+                viewColumnLabels[i] = columnLabels[7 - i];
+            }
+        }
+
+        // Print the board
+        System.out.printf("%s%s%s    %s    %s%s%s%n",
+                SET_BG_COLOR_DARK_GREEN, SET_TEXT_COLOR_LIGHT_GREY, PIXEL,
+                String.join(PIXEL.repeat(5), viewRowLabels),
+                PIXEL, RESET_BG_COLOR, RESET_TEXT_COLOR
+        );
+        int i = 0;
+        for (String row : board) {
+            System.out.printf("%s%s %s %s%s%s %s %s%s%n",
+                    SET_BG_COLOR_DARK_GREEN, SET_TEXT_COLOR_LIGHT_GREY, viewColumnLabels[i], row,
+                    SET_BG_COLOR_DARK_GREEN, SET_TEXT_COLOR_LIGHT_GREY, viewColumnLabels[i],
+                    RESET_TEXT_COLOR, RESET_BG_COLOR
+            );
+            i++;
+        }
+        System.out.printf("%s%s%s    %s    %s%s%s%n",
+                SET_BG_COLOR_DARK_GREEN, SET_TEXT_COLOR_LIGHT_GREY, PIXEL,
+                String.join(PIXEL.repeat(5), viewRowLabels),
+                PIXEL, RESET_BG_COLOR, RESET_TEXT_COLOR
+        );
+
+        return UIState.QUIT;
+    }
+
+
+    private Collection<String> convertBoard(ChessGame game, String role) {
+        Collection<String> board = new ArrayList<>();
+        boolean white = !role.equals("BLACK");
+
+        ChessBoard gameBoard = game.getBoard();
+        for (int i = white ? 1 : 8; white ? i < 9 : i > 0; i += white ? 1 : -1) {
+            StringBuilder row = new StringBuilder();
+            for (int j = white ? 1 : 8; white ? j < 9 : j > 0; j+= white ? 1 : -1) {
+                ChessPiece piece = gameBoard.getPiece(new ChessPosition(9-i, j));
+                if ((i + j) % 2 == 0) {
+                    row.append(SET_BG_COLOR_LIGHT_GREY);
+                } else {
+                    row.append(SET_BG_COLOR_DARK_GREY);
+                }
+
+                // Set the color of the piece
+                if (piece != null) {
+                    if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
+                        row.append(SET_TEXT_COLOR_BLACK);
+                        row.append(blackPieceMap.get(piece.getPieceType()));
+                    } else {
+                        row.append(SET_TEXT_COLOR_WHITE);
+                        row.append(blackPieceMap.get(piece.getPieceType()));
+                    }
+                } else {
+                    if ((i + j) % 2 == 0) {
+                        row.append(SET_TEXT_COLOR_LIGHT_GREY);
+                    } else {
+                        row.append(SET_TEXT_COLOR_DARK_GREY);
+                    }
+                    row.append(blackPieceMap.get(ChessPiece.PieceType.PAWN));
+                }
+            }
+            row.append(RESET_BG_COLOR);
+            row.append(RESET_TEXT_COLOR);
+            board.add(row.toString());
+        }
+
+        return board;
+    }
+
 }
--- client/src/main/java/ui/EscapeSequences.java
+++ client/src/main/java/ui/EscapeSequences.java
@@ -25,10 +25,11 @@ public class EscapeSequences {
     private static final String SET_BG_COLOR = UNICODE_ESCAPE + "[48;5;";
 
     public static final String SET_TEXT_COLOR_BLACK = SET_TEXT_COLOR + "0m";
-    public static final String SET_TEXT_COLOR_LIGHT_GREY = SET_TEXT_COLOR + "242m";
-    public static final String SET_TEXT_COLOR_DARK_GREY = SET_TEXT_COLOR + "235m";
+    public static final String SET_TEXT_COLOR_LIGHT_GREY = SET_TEXT_COLOR + "245m";
+    public static final String SET_TEXT_COLOR_DARK_GREY = SET_TEXT_COLOR + "242m";
     public static final String SET_TEXT_COLOR_RED = SET_TEXT_COLOR + "160m";
     public static final String SET_TEXT_COLOR_GREEN = SET_TEXT_COLOR + "46m";
+    public static final String SET_TEXT_COLOR_DARK_GREEN = SET_TEXT_COLOR + "22m";
     public static final String SET_TEXT_COLOR_YELLOW = SET_TEXT_COLOR + "226m";
     public static final String SET_TEXT_COLOR_BLUE = SET_TEXT_COLOR + "12m";
     public static final String SET_TEXT_COLOR_MAGENTA = SET_TEXT_COLOR + "5m";
@@ -36,8 +37,8 @@ public class EscapeSequences {
     public static final String RESET_TEXT_COLOR = UNICODE_ESCAPE + "[39m";
 
     public static final String SET_BG_COLOR_BLACK = SET_BG_COLOR + "0m";
-    public static final String SET_BG_COLOR_LIGHT_GREY = SET_BG_COLOR + "242m";
-    public static final String SET_BG_COLOR_DARK_GREY = SET_BG_COLOR + "235m";
+    public static final String SET_BG_COLOR_LIGHT_GREY = SET_BG_COLOR + "245m";
+    public static final String SET_BG_COLOR_DARK_GREY = SET_BG_COLOR + "242m";
     public static final String SET_BG_COLOR_RED = SET_BG_COLOR + "160m";
     public static final String SET_BG_COLOR_GREEN = SET_BG_COLOR + "46m";
     public static final String SET_BG_COLOR_DARK_GREEN = SET_BG_COLOR + "22m";
@@ -59,7 +60,9 @@ public class EscapeSequences {
     public static final String BLACK_KNIGHT = " ♞ ";
     public static final String BLACK_ROOK = " ♜ ";
     public static final String BLACK_PAWN = " ♟ ";
+    public static final String PIXEL = "\u2004";
     public static final String EMPTY = " \u2003 ";
+
 
     public static String moveCursorToLocation(int x, int y) { return UNICODE_ESCAPE + "[" + y + ";" + x + "H"; }
 }
